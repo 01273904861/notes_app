928437321236f58eb621b87ec569333a48a0dad9
abdullah mohamed <abdullahmohamed1122002.com>
2024-05-04 00:42:54 +0300

    create item model

---
+++ lib/models/note_item_model.dart
@@ -0,0 +1,12 @@
+class NoteItemModel {
+  final String title;
+  final String subtite;
+  final String date;
+  final int color;
+  const NoteItemModel({
+    required this.date,
+    required this.color,
+    required this.subtite,
+    required this.title,
+  });
+}
