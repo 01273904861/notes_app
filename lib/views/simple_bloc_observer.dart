9ff9c3cc81043f75e4a4372749c5b3d97e4dd5a1
abdullah mohamed <abdullahmohamed1122002.com>
2024-05-26 18:55:27 +0300

    dd

--- lib/views/simple_bloc_observer.dart
+++ lib/views/simple_bloc_observer.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class SimpleBlokObserver implements BlocObserver{
   @override
   void onChange(BlocBase bloc, Change change) {
-   debugPrint('change : $change');
+   debugPrint('changed : $change');
   }
 
   @override
